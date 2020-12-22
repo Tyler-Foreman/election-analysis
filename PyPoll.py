2455fe58f51fa1728cdc5aac282a014ad2ca432d
Tyler-Foreman <75589171+Tyler-Foreman@users.noreply.github.com>
2020-12-22 14:58:30 -0600

    Updates for obtaining candidate name

--- PyPoll.py
+++ PyPoll.py
@@ -5,7 +5,7 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote
 
-# Import packages
+# Import package dependencies
 import csv
 import os
 
@@ -14,6 +14,11 @@ file_to_load = os.path.join("Resources" , "election_results.csv")
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
 
+# 1. Initialize a total vote counter
+total_votes = 0
+# Create an empty list for candidate options
+candidate_options = []
+
 # Open the election results and read the file
 with open(file_to_load) as election_data:
     #Read the file object with the reader function
@@ -21,8 +26,22 @@ with open(file_to_load) as election_data:
     
     # Read and print the header row
     headers = next(file_reader)
-    print(headers)
-    # To do: perform analysis
+    
+    # Print each row in the CSV file
+    for row in file_reader:
+        # 2. Add to the total vote count
+        total_votes +=1
+
+        # Print the candidate name from each row
+        candidate_name = row[2]
+
+        # If the candidate does not match any existing candidate
+        if candidate_name not in candidate_options:
+            # Add the candidate name to the candidate list
+            candidate_options.append(candidate_name)
+
+print(total_votes)
+print(candidate_options)
 
 
 
